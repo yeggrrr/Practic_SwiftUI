6951de43977047a5ac0e1e74976ef527db5fd117
yeggrrr <yjkim981103@gmail.com>
2024-09-02 13:15:41 +0900

    Initial Commit

---
+++ Practic_SwiftUI/Practic_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practic_SwiftUIApp.swift
+//  Practic_SwiftUI
+//
+//  Created by YJ on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Practic_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
