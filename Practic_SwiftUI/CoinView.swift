7dca25ff90b7bb296446e569618f3424775c3201
yeggrrr <yjkim981103@gmail.com>
2024-09-03 20:22:52 +0900

    [Feat] CoinView UI & API 데이터 적용

---
+++ Practic_SwiftUI/CoinView.swift
@@ -0,0 +1,97 @@
+//
+//  CoinView.swift
+//  Practic_SwiftUI
+//
+//  Created by YJ on 9/3/24.
+//
+
+import SwiftUI
+
+struct CoinView: View {
+    @State private var searchText = ""
+    @State private var banner = Banner()
+    @State private var market: Markets = []
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                bannerView()
+                listView()
+            }
+            .navigationTitle("My Money")
+        }
+        .task {
+            UpbitAPI.fetchAllMarket { marketData in
+                market = marketData
+            }
+            
+            do {
+                let result = try await
+                UpbitAPI.fetchAllMarket()
+                market = result
+            } catch {
+                print("error: \(error)")
+            }
+        }
+        .searchable(
+          text: $searchText,
+          placement: .navigationBarDrawer,
+          prompt: "Search.."
+        )
+    }
+    
+    func bannerView() -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(banner.color)
+                .overlay(alignment: .leading) {
+                    Circle()
+                        .fill(.white.opacity(0.3))
+                        .scaleEffect(2)
+                        .offset(x: -50)
+                }
+            
+            VStack(alignment: .leading) {
+                Spacer()
+                Text("나의 소비내역")
+                    .font(.callout)
+                Text(banner.totalFormat)
+                    .font(.title).bold()
+            }
+            .foregroundStyle(.white)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding()
+            
+        }
+        .frame(height: 150)
+        .padding(.horizontal)
+    }
+    
+    func rowView(_ item: Market) -> some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(item.koreanName)
+                    .fontWeight(.bold)
+                Text(item.market)
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+            }
+            Spacer()
+            Text(item.englishName)
+        }
+        .padding(.horizontal, 20)
+        .padding(.vertical, 6)
+    }
+    
+    func listView() -> some View {
+        LazyVStack {
+            ForEach(market, id: \.self) { item in
+                rowView(item)
+            }
+        }
+    }
+}
+
+#Preview {
+    CoinView()
+}
